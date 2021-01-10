feacc12ab05834fc14cf3b65b42f220fd209191f
Faouzi Aitelhara <faouzi.aitelhara@gmail.com>
2021-01-10 02:06:42 +0000

    Fixed TokenPane import

--- src/redux/saga.js
+++ src/redux/saga.js
@@ -1,7 +1,7 @@
 import { all } from 'redux-saga/effects';
 import { authenticateSaga } from '../components/pages/login/saga';
 import { registerSaga } from '../components/pages/register/saga';
-import { resendTokenSaga } from '../components/ui/reset_token/saga';
+import { resendTokenSaga } from '../components/ui/resend_token/saga';
 
 export function* sagas() {
   yield all([authenticateSaga(), registerSaga(), resendTokenSaga()]);
