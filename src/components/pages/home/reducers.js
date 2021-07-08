56da97ad0f2f05b19ae3537f3bd99a7d316a26a5
Faouzi Aitelhara <faouzi.aitelhara@gmail.com>
2021-07-08 22:51:19 +0100

    Fixed

--- src/components/pages/home/reducers.js
+++ src/components/pages/home/reducers.js
@@ -13,6 +13,7 @@ const initialState = {
 export const home_page_products = (state = initialState, action) => {
   switch (action.type) {
     case GET_HOME_PAGE_PRODUCTS_SUCCESS:
+      console.log(action.payload)
       return { ...state, products: action.payload };
     case GET_HOME_PAGE_PRODUCTS_FAILURE:
       return { ...state, errors: action.payload };
--- src/components/pages/submenu/actions.js
+++ src/components/pages/submenu/actions.js
@@ -5,8 +5,7 @@ import {
   GET_PRODUCT_BY_CATEGORY_FAILURE,
 } from './constants';
 
-export const getProducts = (payload) => {
-  console.log('LOADING...')
+export const getProducts = ({ payload }) => {
   return { type: GET_PRODUCT_BY_CATEGORY, payload };
 };
 
