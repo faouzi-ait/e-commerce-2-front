00ba318395f533bca7bf9b07bf9974617339fa80
Faouzi Aitelhara <faouzi.aitelhara@gmail.com>
2021-07-20 20:51:44 +0100

    Fixed variables assignment

--- src/components/ui/ProductDisplay/card/index.js
+++ src/components/ui/ProductDisplay/card/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {} from './styles.module.scss';
 
 function Card({ products, category }) {
-  const { items } = products.data;
+  const { items } = products?.data;
   return (
     <div>
       <div>Card</div>
-      {items.map((item) => (
+      {(items || []).map((item) => (
         <div>{item.quantity}</div>
       ))}
     </div>
