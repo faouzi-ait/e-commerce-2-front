895e0cda234a8b47717455bb24f9ccdb411b0002
Faouzi Aitelhara <faouzi.aitelhara@gmail.com>
2021-08-17 18:20:26 +0100

    FIX for header submenu

--- src/components/ui/header/index.js
+++ src/components/ui/header/index.js
@@ -41,7 +41,7 @@ function Header() {
       };
     });
 
-    let submenuFiltered = category?.items
+    const submenuFiltered = (category || []).items
       ?.filter((item) => item.showOnNav === true)
       .map((label) => {
         return {
