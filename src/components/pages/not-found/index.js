0527786bded5691075b66ff3b0bbbec5dc0a58a1
Faouzi Aitelhara <faouzi.aitelhara@gmail.com>
2021-12-11 00:39:21 +0000

    Added the 404 page

--- src/components/pages/not-found/index.js
+++ src/components/pages/not-found/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import * as cmpStyle from './styles.module.scss';
 
 const NotFound = () => {
-  return <div className={cmpStyle.base}>Not Found</div>;
+  return <div className={cmpStyle.base}>
+    <img src="./images/404.png" alt="404" />
+  </div>;
 };
 
 export default NotFound;
